547e9032a959cdfa0bbf83717048777d4e2db2cf
pedevineau <pierre-etienne.devineau@data.gouv.fr>
2022-07-13 11:21:04 +0200

    Remove Moses

--- pseudo_api/data_ETL.py
+++ pseudo_api/data_ETL.py
@@ -6,9 +6,6 @@ from typing import Callable, List, Tuple
 
 from flair.data import Token, Sentence
 from flair.models import SequenceTagger
-from sacremoses import MosesTokenizer, MosesDetokenizer, MosesPunctNormalizer
-
-moses_detokenize = MosesDetokenizer(lang="fr")
 import stopwatch
 
 sw = stopwatch.StopWatch()
@@ -21,29 +18,22 @@ def create_conll_output(sentences_tagged: List[Sentence]):
     for sent_pred in sentences_tagged:
         tags.extend([s for s in sent_pred.get_spans("ner")])
         for tok_pred in sent_pred:
-            result_str = f"{moses_detokenize.tokenize([tok_pred.text])}\t{tok_pred}\t" \
+            result_str = f"{tok_pred.text}\t{tok_pred}\t" \
                          f"{tok_pred.start_pos}\t{tok_pred.end_pos}"
             conll_str += result_str + "\n"
         conll_str += "\n"
     return conll_str, Counter(tags)
 
 
-def prepare_output(text: str, tagger: SequenceTagger, word_tokenizer=None, output_type: str = "pseudonymized"):
+def prepare_output(text: str, tagger: SequenceTagger, output_type: str = "pseudonymized"):
     stats_dict = {}
     with sw.timer("root"):
-        if not word_tokenizer:
-            tokenizer = MOSES_TOKENIZER
-        else:
-            tokenizer = word_tokenizer
-
-        # text = [t.strip() for t in text.split("\n") if t.strip()]
         text_sentences = [Sentence(t.strip(), use_tokenizer=True)
                           for t in text.split("\n") if t.strip()]
         with sw.timer('model_annotation'):
             tagger.predict(sentences=text_sentences,
                                               mini_batch_size=32,
                                               embedding_storage_mode="none",
-                                              # use_tokenizer=tokenizer,
                                               verbose=True)
 
         if output_type == "conll":
@@ -135,8 +125,7 @@ def create_pseudonymized_text(sentences_tagged: List[Sentence]):
     # Iterate over the modified sentences to recreate the text (pseudonymized)
     pseudonymized_str = ""
     for sent in sentences_pseudonymized:
-        detokenized_str = moses_detokenize.detokenize([t.text for t in sent.tokens],
-                                                      return_str=True)
+        detokenized_str = " ".join([t.text for t in sent.tokens]),
         pseudonymized_str += detokenized_str + "\n\n"
 
     return pseudonymized_str, tag_stats
@@ -148,92 +137,3 @@ def create_api_output(sentences_tagged: List[Sentence]) -> Tuple[str, str]:
     pseudonymized_str = create_pseudonymized_text(sentences_tagged=sentences_tagged)
 
     return tagged_str, pseudonymized_str
-
-
-# ENTITIES = {"PER_PRENOM": "PRENOM", "PER_NOM": "NOM", "LOC": "ADRESSE"}
-
-class MosesTokenizerSpans(MosesTokenizer):
-    def __init__(self, lang="en", custom_nonbreaking_prefixes_file=None):
-        MosesTokenizer.__init__(self, lang=lang,
-                                custom_nonbreaking_prefixes_file=custom_nonbreaking_prefixes_file)
-        self.lang = lang
-
-    def span_tokenize(
-            self,
-            text,
-            aggressive_dash_splits=False,
-            escape=True,
-            protected_patterns=None,
-    ):
-        # https://stackoverflow.com/a/35634472
-        import re
-        detokenizer = MosesDetokenizer(lang=self.lang)
-        tokens = self.tokenize(text=text, aggressive_dash_splits=aggressive_dash_splits,
-                               return_str=False, escape=escape,
-                               protected_patterns=protected_patterns)
-        tail = text
-        accum = 0
-        tokens_spans = []
-        for token in tokens:
-            detokenized_token = detokenizer.detokenize(tokens=[token],
-                                                       return_str=True,
-                                                       unescape=escape)
-            escaped_token = re.escape(detokenized_token)
-
-            m = re.search(escaped_token, tail)
-            tok_start_pos, tok_end_pos = m.span()
-            sent_start_pos = accum + tok_start_pos
-            sent_end_pos = accum + tok_end_pos
-            accum += tok_end_pos
-            tail = tail[tok_end_pos:]
-
-            tokens_spans.append((detokenized_token, (sent_start_pos, sent_end_pos)))
-        return tokens_spans
-
-
-def build_moses_tokenizer(tokenizer: MosesTokenizerSpans,
-                          normalizer: MosesPunctNormalizer = None) -> Callable[[str], List[Token]]:
-    """
-    Wrap Spacy model to build a tokenizer for the Sentence class.
-    :param model a Spacy V2 model
-    :return a tokenizer function to provide to Sentence class constructor
-    """
-    try:
-        from sacremoses import MosesTokenizer
-        from sacremoses import MosesPunctNormalizer
-    except ImportError:
-        raise ImportError(
-            "Please install sacremoses or better before using the Spacy tokenizer, otherwise you can use segtok_tokenizer as advanced tokenizer."
-        )
-
-    moses_tokenizer: MosesTokenizerSpans = tokenizer
-    if normalizer:
-        normalizer: MosesPunctNormalizer = normalizer
-
-    def tokenizer(text: str) -> List[Token]:
-        if normalizer:
-            text = normalizer.normalize(text=text)
-        doc = moses_tokenizer.span_tokenize(text=text, escape=False)
-        previous_token = None
-        tokens: List[Token] = []
-        for word, (start_pos, end_pos) in doc:
-            word: str = word
-            token = Token(
-                text=word, start_position=start_pos, whitespace_after=True
-            )
-            tokens.append(token)
-
-            if (previous_token is not None) and (
-                    token.start_pos - 1
-                    == previous_token.start_pos + len(previous_token.text)
-            ):
-                previous_token.whitespace_after = False
-
-            previous_token = token
-        return tokens
-
-    return tokenizer
-
-
-MOSES_TOKENIZER = build_moses_tokenizer(tokenizer=MosesTokenizerSpans(lang="fr"),
-                                        normalizer=MosesPunctNormalizer(lang="fr"))
